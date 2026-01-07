1151f78198eceaa46c9b53ddcd4888ed95698614
Pedro de Souza <98967887+galvaomaisti@users.noreply.github.com>
2026-01-07 11:08:58 -0300

    Revise product menu with updated items
    Updated product list with new burger names and prices.
    
    Signed-off-by: Pedro de Souza <98967887+galvaomaisti@users.noreply.github.com>

--- products.js
+++ products.js
@@ -5,69 +5,76 @@
 const menuOptions = JSON.parse(localStorage.getItem("products")) || [
   {
     id: 1,
-    name: "Burger Clássico",
-    price: 25,
+    name: "Burger Delta",
+    price: 18.9,
     category: "hamburgers",
     src: "./src/amburger.jpg"
   },
   {
     id: 2,
-    name: "Burger Duplo",
-    price: 32,
+    name: "Burger Monagas",
+    price: 21.90,
     category: "hamburgers",
     src: "./src/Haburguer-Duplo.jpg"
   },
   {
     id: 3,
-    name: "X-Bacon",
-    price: 28,
+    name: "Burger Zulia",
+    price: 23.90,
     category: "hamburgers",
     src: "./src/xbacon.png"
   },
-  {
+   {
     id: 4,
+    name: "Burger Anzoátegui",
+    price: 25.90,
+    category: "hamburgers",
+    src: "./src/Haburguer-Duplo.jpg"
+  },
+    {
+    id: 5,
+    name: "Burger Bolivar",
+    price: 38.90,
+    category: "hamburgers",
+    src: "./src/Haburguer-Duplo.jpg"
+  },
+  {
+    id: 6,
     name: "Refrigerante",
     price: 7,
     category: "bebidas",
     src: "./src/Bebidas.jpg"
   },
   {
-    id: 5,
-    name: "Milkshake",
-    price: 12,
-    category: "bebidas",
-    src: "./src/Milkshake.jpeg"
-  },
-  {
-    id: 6,
+    id: 7,
     name: "Porção de Batata Frita",
     price: 15,
     category: "porcoes",
     src: "./src/Batata-Frita.jpg"
   },
   {
-    id: 7,
+    id: 8,
     name: "Combo Família",
     price: 60,
     category: "porcoes",
     src: "./src/Combo-Supremo.jpg"
   },
   {
-    id: 8,
+    id: 9,
     name: "Vegano Burger",
     price: 28,
     category: "veganos",
     src: "./src/xvegan.png"
   },
   {
-    id: 9,
+    id: 10,
     name: "xSalada Vegano",
     price: 28,
     category: "veganos",
     src: "./src/xsalada.jpeg"
   },
   {
-    id: 10,
+    id: 11,
     name: "Burger Duplo Smash",
     price: 35,
     promoPrice: 29,
@@ -76,3 +83,4 @@ const menuOptions = JSON.parse(localStorage.getItem("products")) || [
     featured: true
   }
 ]
+
